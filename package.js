861f88547e8c63fe9e15ee70f963bd23562fad21
Pierre Wieser <pwieser@trychlos.org>
2023-09-11 03:13:25 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:modal-info',
-    version: '1.4.0',
+    version: '1.4.1-rc',
     summary: 'A Bootstrap-based Meteor package which displays object informations in a modal dialog',
     git: 'https://github.com/trychlos/pwix-modal-info',
     documentation: 'README.md'
